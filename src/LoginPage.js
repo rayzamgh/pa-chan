bfb9cb7c212c67127d74e5971bb1bfa374d82799
Rayza Mahendra <rayzaganteng@gmail.com>
2023-04-29 10:19:41 +0700

    login page

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,17 +1,17 @@
-// src/LoginPage.js
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { firebase} from './firebase';
+import { firebase } from './firebase';
+import './LoginPage.css';
 
 function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const navigate = useNavigate();
-  
+
   const user = firebase.auth().currentUser;
 
-  console.log(user)
+  console.log(user);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -25,12 +25,33 @@ function LoginPage() {
     }
   };
 
+  const logo_url = `${process.env.PUBLIC_URL}/static/RM_LOGO-DEFINITIVE.jpeg`
+
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-      <button type="submit">Login</button>
-    </form>
+    <div className="login-container">
+      <div className="login-box">
+        <div className="logo-container">
+          <img src={logo_url} alt="Logo" className="logo" />
+        </div>
+        <form onSubmit={handleSubmit} className="login-form">
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+            className="login-input"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            className="login-input"
+          />
+          <button type="submit" className="login-button">
+            Sign In
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
 
